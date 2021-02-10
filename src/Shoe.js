3cf3ce75f9810cf46c605ea2164b8926d0ab2e8e
iannakim <dear.hyunji@gmail.com>
2021-02-10 00:43:08 -0500

    price variation complete

--- src/Shoe.js
+++ src/Shoe.js
@@ -5,14 +5,14 @@ class Shoe extends React.Component {
 
   render() {
 
-    let { title, price, featured_image, variants } = this.props.shoe
+    let { title, price, price_min, price_max, featured_image, variants, compare_at_price } = this.props.shoe
 
     let name = title.split('-')[0]
     let desc = title.split('-')[1]
     let num = variants.length
 
     return (
-        <div class="col">
+        <div className="col">
           <div className="shoeTile">
             <img 
               src={featured_image}
@@ -20,10 +20,10 @@ class Shoe extends React.Component {
               className="shoeImage"
             />
             <div className="shoeInfo">
-              <p>{name}</p>
+              <h3>{name}</h3>
               <p>{desc}</p>
-              <p>{num} {} Colors available</p>
-              <p>${price/100}</p>
+              <p> {num} { num > 1 ? `Colors` : `Color`} Available</p>
+              { compare_at_price ? <div><span className="price-min"> ${price_min/100} </span> <span className="price-max"> ${price_max/100} </span></div> : <span className="price">${price/100}</span>} 
             </div>
           </div>
         </div>
